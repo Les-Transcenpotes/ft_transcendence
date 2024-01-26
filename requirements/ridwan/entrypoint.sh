c5eee5d8f08ad54e55b64953d57d3e2088b43a8f
Arthur Ascedu <aascedu@z3r4p1.42lyon.fr>
2024-01-26 15:03:43 +0100

    Changed to Apache2 server. ModSecurity is installed but not config

--- requirements/ridwan/entrypoint.sh
+++ requirements/ridwan/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # entrypoint.sh
 
-#nginx -V | awk -F 'configure arguments: ' '{print $2}' 
+nginx -V 2>&1 | awk -F 'configure arguments: ' '{printf $2}'
 #/opt/test < awk -F 'configure arguments: ' '{print $2}' > /opt/test
