02e9879eb63d2e3a5c2348070fc5de7c62baaf68
hgeffroy <henri.geffroy@gmail.com>
2024-01-26 13:56:53 +0100

    Setup Match and tournament models

--- requirements/coubertin/coubertin_project/tournament/models.py
+++ requirements/coubertin/coubertin_project/tournament/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# player1 and player2 need to be unique identifier
+class Match(models.Model):
+    player1 = models.CharField()
+    player2 = models.CharField()
+    result = {player1: 0, player2: 0}
+
+class Tournament(models.Model):
+    matches = models.ManyToManyField(Match)
+
