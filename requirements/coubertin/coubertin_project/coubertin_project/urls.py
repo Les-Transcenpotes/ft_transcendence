8cc68af399487c41ac56293ba688196d03a30ea8
hgeffroy <henri.geffroy@gmail.com>
2024-01-30 10:32:33 +0100

    create tournament base front

--- requirements/coubertin/coubertin_project/coubertin_project/urls.py
+++ requirements/coubertin/coubertin_project/coubertin_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from create_tournament import views as create_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("tournament.urls"))
+    path('createtournament', create_views.createTournament, name='createtournament'),
+    path('', include("tournament.urls")),
 ]
--- requirements/coubertin/coubertin_project/tournament/views.py
+++ requirements/coubertin/coubertin_project/tournament/views.py
@@ -5,7 +5,7 @@ from .classes import *
 ################ Donnes a recuperer via les request ################
 
 testTournament = Tournament(name = 'testTournament', maxPlayers = 8, password = 'test')
-testUser1 = Player()
+testUser1 = Player(name = 'Brieuc')
 
 ####################################################################
 
@@ -19,5 +19,4 @@ def addUserToTournament(request):
     return HttpResponse('User added')
 
 def startTournament(request): # Generate matches here.
-    # queryset is iterable!
     pass
