fc7f920b164f3a1a24ea09191d052f346313f44c
hgeffroy <henri.geffroy@gmail.com>
2024-01-30 11:21:52 +0100

    solve conflicts

--- requirements/coubertin/coubertin_project/create_tournament/apps.py
+++ requirements/coubertin/coubertin_project/create_tournament/apps.py
@@ -1,12 +1,6 @@
 from django.apps import AppConfig
 
-
-<<<<<<<< HEAD:requirements/coubertin/coubertin_project/create_tournament/apps.py
 class CreateTournamentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'create_tournament'
-========
-class MatchmakingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'matchmaking'
->>>>>>>> 01422521c4e0805186cce2bd72906179366f16bb:requirements/cupidon/cupidon_project/matchmaking/apps.py
+
