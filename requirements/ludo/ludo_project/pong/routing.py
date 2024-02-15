6497916f094009add34fe7d8e95580dd68311baf
hgeffroy <henri.geffroy@gmail.com>
2024-02-15 08:39:46 +0100

    Async web socket

--- requirements/ludo/ludo_project/pong/routing.py
+++ requirements/ludo/ludo_project/pong/routing.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
 
-from . import players
+from classes import PongGameConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/pong/(?P<room_name>\w+)/$", players.PongPlayer.as_asgi()),
+    re_path(r"ws/pong/(?P<room_name>\w+)/$", PongGameConsumer.as_asgi()),
 ]
