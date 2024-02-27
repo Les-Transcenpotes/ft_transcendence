ee8652bbb7d623ea6fe4c6b9e5df8db6a98057e5
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-02-27 12:03:34 +0100

    fix(Petrus): suppress the 'bad' view

--- requirements/petrus/petrus_project/signin/urls.py
+++ requirements/petrus/petrus_project/signin/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import signinView, signupView, refreshView, view_db, bad
+from .views import signinView, signupView, refreshView, view_db
 
 urlpatterns = [
     path("view-db", view_db),
-    path("bad", bad),
     path("signin", signinView.as_view()),
     path("signup/<str:string>", signupView.as_view()),
     path("JWT_refreshment", refreshView.as_view())
