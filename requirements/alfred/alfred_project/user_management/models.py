197b75af64ea91dc2167468128170acc8b44a918
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-02-27 12:36:21 +0100

    feat(Petrus and Alfred): nickname defined to 16 chars and redef of __str__ of a User

--- requirements/alfred/alfred_project/user_management/models.py
+++ requirements/alfred/alfred_project/user_management/models.py
@@ -6,19 +6,22 @@ from django.db import models
 
 class User(models.Model):
     unique_id = models.BigAutoField(primary_key=True)
-    firstName = models.CharField(max_length=10)
-    lastName = models.CharField(max_length=20)
-    nick = models.CharField(max_length=12, unique=True)
+    nick = models.CharField(max_length=16, unique=True)
     email = models.EmailField()
     # avatar = models.ImageField('avatars/')
-    friends = models.ManyToManyField('self', blank=True)
+    # friends = models.ManyToManyField('self', blank=True)
     objects = models.Manager()
+
 
     def __init__(self, *args, **kwargs):
         super(User, self).__init__(*args, **kwargs)
 
     def __str__(self):
-        return self.firstName.__str__()
+        return f"""
+                ${self.nick.__str__()}
+                ${self.email.__str__()}
+                ${self.unique_id.__str__()}
+                """
 
     def to_dict(self):
         return {
