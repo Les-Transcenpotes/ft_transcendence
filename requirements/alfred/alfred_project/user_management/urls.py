ed154a39161807a8b3565590193cfe8767e36239
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-02-29 15:50:14 +0100

    feat(Alfred): add urls

--- requirements/alfred/alfred_project/user_management/urls.py
+++ requirements/alfred/alfred_project/user_management/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from user_management.views import createUserView, personalInfoView, all_client, createUser
+from user_management.views import createUserView, personalInfoView, all_client, createUser, clientInfoIdView, friendView
 
 urlpatterns = [
         path("create-user", createUserView.as_view()),
         path("personal-info", personalInfoView.as_view()),
-        path("id/<int:id>", all_client),
+        path("id/<int:id>", clientInfoIdView.as_view()),
+        path("add-friendi/<int:id>", friendView.as_view()),
         path("test", createUser),
         path("view-db", all_client)
 ]
