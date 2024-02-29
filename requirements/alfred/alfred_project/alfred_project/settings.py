5d1c30cfc29d02ae098bf5b0ed3e0935b2e03cbb
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-02-29 16:33:15 +0100

    feat(Alfred): exempt csrf token

--- requirements/alfred/alfred_project/alfred_project/settings.py
+++ requirements/alfred/alfred_project/alfred_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.views.decorators.csrf.csrf_exempt',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
