1cf78eb04133a2fe4af5d1f223e13e5bc7c1d73f
hgeffroy <henri.geffroy@gmail.com>
2024-03-04 10:07:02 +0100

    build(ludo): can now play in different rooms in parallel

--- requirements/ludo/ludo_project/pong/urls.py
+++ requirements/ludo/ludo_project/pong/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.pong, name="pong"),
+    path("<str:roomName>/", views.pong, name="pong"),
 ]
