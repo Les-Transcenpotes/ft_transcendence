2f301f292924032af041171024a576830512ae9c
Thea Wang <twang@student.42lyon.fr>
2024-03-11 11:08:12 +0100

    style ( tools/scripts* ) : changing compiling rules for migrations scripts

--- tools/migrate.sh
+++ tools/migrate.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+COLOR_RED='\e[1;31m'
+COLOR_GREEN='\e[1;32m'
+COLOR_BLUE='\e[1;34m'
+COLOR_RESET='\e[0m'
+
 for container in "$@"
 do
-     echo "Exécution des migrations sur $container"
+     echo -e "${COLOR_BLUE}Copying shared_code : ${COLOR_RESET}$container"
+     cp ./requirements/shared_code/* ./requirements/$container/*/shared/
+done
+
+echo -e -n "\n"
+
+for container in "$@"
+do
+     echo -e "${COLOR_RED}Executing migrations for : ${COLOR_RESET}$container"
      python3 ./requirements/$container/*/manage.py makemigrations
      python3 ./requirements/$container/*/manage.py migrate
-     echo -e "\n"
+     echo -e -n "\n"
 done
 
