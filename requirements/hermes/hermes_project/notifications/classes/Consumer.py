85681255dadfc06099380b09d8c3af480b91785d
hgeffroy <henri.geffroy@gmail.com>
2024-03-22 15:01:40 +0100

    build(hermes): basic setup of hermes' websockets

--- requirements/hermes/hermes_project/notifications/classes/Consumer.py
+++ requirements/hermes/hermes_project/notifications/classes/Consumer.py
@@ -5,26 +5,49 @@ class Consumer(AsyncWebsocketConsumer):
     async def connect(self):
 
         # Join room group
-        await self.channel_layer.group_add("matchmakingRoom", self.channel_name)
-
+        await self.channel_layer.group_add("notificationRoom", self.channel_name)
         await self.accept()
+        self.name = 'test'
 
     async def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard("matchmakingRoom", self.channel_name)
+        await self.channel_layer.group_discard("notificationRoom", self.channel_name)
 
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        state = text_data_json['state']
+        type = text_data_json['state']
+        source = text_data_json['source']
+        target = text_data_json['target']
         
         # Send message to room group
         await self.channel_layer.group_send(
-            "matchmakingRoom", {
-                "type": state
+            "notificationRoom", {
+                "type": type,
+                "target": target,
+                "source": source,
             }
         )
 
-    # Receive message from room group (mettre un _ dans le nom de la room entre les deux joueurs)
-    async def Ready(self, event):
-        pass
+    # Receive message from room group
+    async def FriendRequest(self, event):
+        if event['target'] == self.name:
+            await self.send (text_data=json.dumps({
+            "type": "friendRequest",
+            "source": event['source'],
+        }))
+
+    async def GameInvite(self, event):
+        if event['target'] == self.name:
+            await self.send (text_data=json.dumps({
+            "type": "gameInvite",
+            "source": event['source'],
+        }))
+
+    async def TournamentInvite(self, event):
+        if event['target'] == self.name:
+            await self.send (text_data=json.dumps({
+            "type": "tournamentInvite",
+            "source": event['source'],
+        }))
+
--- requirements/hermes/hermes_project/notifications/views.py
+++ requirements/hermes/hermes_project/notifications/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 
 def notifications(request):
     pass
+
+# View for game when a game is starting
