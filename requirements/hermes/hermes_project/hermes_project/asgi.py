3236f40882d7bef2d7650ce43a939c1979c47727
hgeffroy <henri.geffroy@gmail.com>
2024-03-22 14:20:03 +0100

    build(hermes): basic setup

--- requirements/hermes/hermes_project/hermes_project/asgi.py
+++ requirements/hermes/hermes_project/hermes_project/asgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from notifications.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hermes_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coubertin_project.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))),
+})
