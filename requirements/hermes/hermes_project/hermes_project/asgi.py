8fff0972c0ff097a3df3dd14da633640a8a4d833
Théa Wang <109480505+Zwhea@users.noreply.github.com>
2024-03-26 10:45:02 +0100

    fix(hermes): asgi app

--- requirements/hermes/hermes_project/hermes_project/asgi.py
+++ requirements/hermes/hermes_project/hermes_project/asgi.py
@@ -15,7 +15,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from notifications.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coubertin_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hermes_project.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
