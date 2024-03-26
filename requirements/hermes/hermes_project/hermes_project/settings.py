ad9485846dbfa9da2567a4ca99a068e3cc9b23fa
Théa Wang <109480505+Zwhea@users.noreply.github.com>
2024-03-26 10:47:48 +0100

    fix(hermes): settings

--- requirements/hermes/hermes_project/hermes_project/settings.py
+++ requirements/hermes/hermes_project/hermes_project/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ASGI_APPLICATION = "cupidon_project.asgi.application"
+ASGI_APPLICATION = "hermes_project.asgi.application"
 
 CHANNEL_LAYERS = {
     "default": {
